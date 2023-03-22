0364393fe8b63b84f3eea93af9757ea03ea83c6f
lyapkin <lyapkin992@gmail.com>
2023-03-22 10:45:11 +0500

    Changes for test deployment

--- messenger/messenger/settings.py
+++ messenger/messenger/settings.py
@@ -22,7 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', from .ENV import key.SECRET_KEY)
+try:
+    from .ENV import key
+    SECRET_KEY = key.SECRET_KEY
+except ImportError:
+    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
