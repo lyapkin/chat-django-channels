21c6765477e673381cbc04c3c35f734022e6a2d1
lyapkin <lyapkin992@gmail.com>
2023-03-22 10:34:15 +0500

    Changes for test deployment

--- messenger/messenger/settings.py
+++ messenger/messenger/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-from .ENV import key
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', key.SECRET_KEY)
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', from .ENV import key.SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
