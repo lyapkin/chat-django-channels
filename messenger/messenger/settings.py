ed786fc47d9c5f42ae84d9a1d035f05d955947c5
lyapkin <lyapkin992@gmail.com>
2023-03-22 19:05:06 +0500

    Changes for test deployment

--- messenger/messenger/settings.py
+++ messenger/messenger/settings.py
@@ -45,9 +45,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://localhost:3000',
-    'https://127.0.0.1:3000',
-    'https://social-hole-chat.up.railway.app/',
-    'http://social-hole-chat.up.railway.app/'
+    'https://127.0.0.1:3000'
 ]
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
@@ -176,3 +174,6 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels.layers.InMemoryChannelLayer"
     }
 }
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
