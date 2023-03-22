d39bc32d1ef5ca6fb4b427122d1cf199b7934c2f
lyapkin <lyapkin992@gmail.com>
2023-03-22 18:50:42 +0500

    Changes for test deployment

--- messenger/messenger/settings.py
+++ messenger/messenger/settings.py
@@ -45,6 +45,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'https://localhost:3000',
     'https://127.0.0.1:3000',
+    'https://social-hole-chat.up.railway.app/'
 ]
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
