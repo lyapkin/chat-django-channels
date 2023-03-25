b861c82e6fcf176369b1310d611d87f35b16b4eb
lyapkin <lyapkin992@gmail.com>
2023-03-25 13:57:50 +0500

    refactor constants.js

--- logreg-front/src/constants.js
+++ logreg-front/src/constants.js
@@ -1,5 +1,7 @@
+const env = process.env.NODE_ENV
+
 export const MOBILE_VERSION_WIDTH = 600
 
-export const BASE_HTTP_URL = 'http://127.0.0.1:8000'
+export const BASE_HTTP_URL = env === 'development' ? 'http://127.0.0.1:8000' : 'https://social-hole-chat.up.railway.app/'
 
-export const BASE_WS_URL = 'ws://127.0.0.1:8000'
+export const BASE_WS_URL = env === 'development' ? 'ws://127.0.0.1:8000' : 'wss://social-hole-chat.up.railway.app/'
